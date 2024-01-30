e84298c07f1b720acaeb352a7844355fbae783c4
alvaro lopes <alvarolopes2020@protonmail.com>
2024-01-29 21:54:11 -0300

    test: created sql query test

--- src/core/infra/ParkingLotRepositorySQL.ts
+++ src/core/infra/ParkingLotRepositorySQL.ts
@@ -7,14 +7,14 @@ export default class ParkingLotRepositorySQL implements ParkingLotRepository {
 
     async getParkingLot(code: string): Promise<ParkingLot | undefined> {
         const parkingLotData =
-            await database.oneOrNone("select *, (select count(*) from example.parked_car pc where pc.code = pl.code) as occupied_spaces from example.parking_lot where code = $1", [code]);
+            await database.oneOrNone("select *, (select count(*) from parked_car pc where pc.code = pl.code) as occupied_spaces from parking_lot pl where code = $1", [code]);
 
         return ParkingLotAdapter.create(parkingLotData.code, parkingLotData.capacity, parkingLotData.openHour, parkingLotData.closeHour, parkingLotData.occupied_spaces);
     }
 
     async saveParkedCar(code: string, plate: string, date: Date): Promise<void> {
         const parkingLotData =
-            await database.oneOrNone("insert into example.parked_car (code, plate, date) values ($1, $2, $3)", [code, plate, date]);
+            await database.oneOrNone("insert into parked_car (code, plate, date) values ($1, $2, $3)", [code, plate, date]);
     }
 
 }
